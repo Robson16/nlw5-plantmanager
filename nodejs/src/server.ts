3330e260c5f8d9e0cc6e1363cc40a28d5e1aeff6
Robson <robhenrod@gmail.com>
2021-07-12 19:56:43 -0300

    NodeJS - Day 1

---
+++ nodejs/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.json({
+    message: 'Olá mundo'
+  });
+})
+
+app.listen(3333, () => {
+  console.log('Server is running on port 3333');
+});
